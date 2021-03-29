01e7689bfbc8bc07b8cca9ef02bf0ea7f6a9fa80
860430 <bert.deterd2@stedin.net>
2021-03-29 10:16:28 +0200

    init

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,20 @@
 #!/usr/bin/env node
 
-//const program = require('commander')
 const { Command } = require('commander');
+const fiori = require('../lib/fiori/fiori');
+const { description, version } = require('../package.json')
+
 const program = new Command();
 
-//import { create } from '../lib/create.mjs'
-const create = require('../lib/create');
-
-const { description, version } = require('../package.json')
-//
 program
  .description(description)
  .version(version, '-v, - version')
  
  program
-    .command('create <name>') // sub-command name
-    .description('create cf mta app') // command description
-    .action(function (name) {
-        create(name);
+    .command('fiori') 
+    .description('create cf mta app fiori style') 
+    .action( () => {
+        fiori();
     });
 
-
-// allow commander to parse `process.argv`
 program.parse(process.argv);
--- lib/cmd/create.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#npx create-react-app $1
-
-ls -la
