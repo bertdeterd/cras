686e7077f349b92684985e059bab6c3cef553659
860430 <bert.deterd2@stedin.net>
2021-03-30 21:12:45 +0200

    init

--- bin/index.js
+++ bin/index.js
@@ -3,18 +3,46 @@
 const { Command } = require('commander');
 const fiori = require('../lib/fiori/fiori');
 const { description, version } = require('../package.json')
+const inquirer = require('inquirer');
 
 const program = new Command();
 
 program
- .description(description)
- .version(version, '-v, - version')
- 
- program
-    .command('fiori <appname>') 
-    .description('create cf mta app fiori style') 
-    .action( (appname) => {
-        fiori(appname);
+    .description(description)
+    .version(version, '-v, - version')
+
+program
+    .command('fiori')
+    .description('create cf mta app fiori style')
+    .action((appname) => {
+
+        var questions = [
+            {
+                type: 'input',
+                name: 'appname',
+                message: "Application Name",
+            },
+            {
+                type: 'input',
+                name: 'appid',
+                message: "Unique Technical Application ID (max 3 char.)",
+                validate: function (value) {
+                    var pass = value.match(/^[a-z]{2,3}$/i);
+                    if (pass) {
+                      return true;
+                    }
+                    return 'Please enter a short ID (max 3 characters)';
+                  },
+            }
+        ];
+
+        inquirer.prompt(questions).then((answers) => {
+            //console.log(JSON.stringify(answers, null, '  '));
+            fiori(answers);
+        });
+
+
+
     });
 
 program.parse(process.argv);
