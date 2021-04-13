0b7abc6e0b418365ad779c2d00aaa9d6cafb8283
860430 <bert.deterd2@stedin.net>
2021-04-13 12:14:52 +0200

    init

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,8 @@
 
 const { Command } = require('commander');
 const fiori = require('../lib/fiori/fiori');
+const vue = require('../lib/vue/vue');
+
 const { description, version } = require('../package.json')
 const inquirer = require('inquirer');
 
@@ -43,5 +45,39 @@ program
 
 
     });
+
+    
+program
+.command('vue')
+.description('create cf mta app vue vuetify style')
+.action((appname) => {
+
+    var questions = [
+        {
+            type: 'input',
+            name: 'appname',
+            message: "Application Name",
+        },
+        {
+            type: 'input',
+            name: 'appid',
+            message: "Unique Technical Application ID (max 3 char.)",
+            validate: function (value) {
+                var pass = value.match(/^[a-z]{2,3}$/i);
+                if (pass) {
+                  return true;
+                }
+                return 'Please enter a short ID (max 3 characters)';
+            },
+        }
+    ];
+
+    inquirer.prompt(questions).then((answers) => {
+        vue(answers);
+    });
+
+
+
+});
 
 program.parse(process.argv);
